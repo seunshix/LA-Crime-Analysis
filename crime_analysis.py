7d64b3d7fde50392da0ae24b0f45f0b987f1b0af
seunshix <adexseun13@Gmail.com>
2022-12-16 11:43:53 -0800

    first commit

---
+++ crime_analysis.py
@@ -0,0 +1,9 @@
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+
+import Flask
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
